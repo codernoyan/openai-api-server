71311cfed1c2071dd519ae8ecc86f2b54885d4f5
codernoyan <coder.noyan@gmail.com>
2023-01-29 12:14:16 +0600

    bugs fixed

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.use(express.json());
 app.use('/openai', require('./routes/openaiApiRoutes'))
 
 // default message
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
   res.send('OpenAI API Server is running');
 });
 
 // listening app on server
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
