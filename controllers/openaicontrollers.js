2ff32d76a6740a36aa71e131d887eff671edef28
codernoyan <coder.noyan@gmail.com>
2023-01-29 11:31:25 +0600

    added vercel config file

--- controllers/openaicontrollers.js
+++ controllers/openaicontrollers.js
@@ -1,19 +1,18 @@
 const { Configuration, OpenAIApi } = require("openai");
-
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
 
 // generate text function
 const generateText = async (req, res) => {
+  const { prompt } = req.body;
   try {
     const response = await openai.createCompletion({
-      // prompt: "create a wonderful message for client",
-      // max_tokens: 1024,
       model: "text-davinci-003",
-      prompt: "create a web logo with computer",
-      max_tokens: 1024,
+      prompt,
+      max_tokens: 75,
       temperature: 0,
     });
     const createdText = response.data.choices[0].text;
